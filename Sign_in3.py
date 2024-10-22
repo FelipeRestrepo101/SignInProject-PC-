c14770bd688d3a84013e385e5536aefcc9d939a6
Felipe Restrepo <me@example.com>
2024-10-22 14:12:40 -0700

    added SQlite3 db

--- Sign_in3.py
+++ Sign_in3.py
@@ -1,13 +1,33 @@
 import tkinter as tk
 import datetime
-
+import sqlite3 as sql
 
 
 def get_input():
     user_input_Name = entry_label_Name.get()
     user_input_Email = entry_label_Email.get()
 
-    print(user_input_Email, ',',  user_input_Name, ',', datetime.date.today())
+    # start of db code
+
+    #upon clicking the  'sign in' button this script will now create the database file 'usersDB.db' in the same directory you have your .py script, 
+    #if the database doesn't already exist. Otherwise it will access the existing 'usersDB.db' file, and insert the sign in entry as a new record. 
+    conn = sql.connect('usersDB.db')
+    c = conn.cursor()
+
+
+    c.execute('''
+            CREATE TABLE IF NOT EXISTS users
+            (name STRING, age INT, date STRING)
+              ''')
+    
+    c.execute('''
+            INSERT INTO users (name, age, date) VALUES (?, ?, ?)
+              ''', (user_input_Name, user_input_Email, str(datetime.date.today()))
+             )
+    
+    conn.commit()
+    # end of db code
+    
 
     entry_label_Name.delete(0, tk.END)  # Clear the entry field
     entry_label_Email.delete(0, tk.END)  
